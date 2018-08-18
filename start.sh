9e60552f3b482645460a1b5590b512edf3a3e002
rs-sliske <rs-sliske@users.noreply.github.com>
2018-08-18 12:23:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,34 +1,30 @@
-    #!/usr/bin/env bash
+#!/usr/bin/with-contenv sh
+set -e;
 
-    set -e
+role=${CONTAINER_ROLE:-app}
+env=${APP_ENV:-production}
 
-    role=${CONTAINER_ROLE:-app}
-    env=${APP_ENV:-production}
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /app && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+fi
 
-    if [ "$env" != "local" ]; then
-        echo "Caching configuration..."
-        (cd /app && php artisan config:cache && php artisan route:cache && php artisan view:cache)
-    fi
+if [ "$role" = "php" ]; then
 
-    if [ "$role" = "php" ]; then
+    /usr/sbin/php-fpm${PHP_VERSION} -R --nodaemonize
+    echo "running php server"
 
-        exec php-fpm
-        echo "running php server"
-        exit 1
+elif [ "$role" = "app" ]; then
+    rsyslogd
+    echo "Running cron"
+    cron -f -L15
 
-    elif [ "$role" = "app" ]; then
-        rsyslogd
-        echo "Running cron"
-        exec cron -f -L15
-        exit 1
+elif [ "$role" = "queue" ]; then
 
-    elif [ "$role" = "queue" ]; then
+    echo "Queue role"
+    (cd /app && php artisan horizon)
 
-        echo "Queue role"
-        (cd /app && php artisan horizon)
-        exit 1
-
-    else
-        echo "Could not match the container role \"$role\""
-        exit 1
-    fi
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
